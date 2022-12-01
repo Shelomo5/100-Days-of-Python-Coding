63cbb6d5c09a1de518c8da95336feda390e46dab
Shelomo Bichindaritz <shelomo@att.net>
2022-11-30 19:16:51 -0800

    Update main.py

--- Day45- Web Scraping (Beautiful Soup)/Final Project - scraping 100 movies to watch/main.py
+++ Day45- Web Scraping (Beautiful Soup)/Final Project - scraping 100 movies to watch/main.py
@@ -3,7 +3,6 @@ from bs4 import BeautifulSoup
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
-# Write your code below this line 👇
 
 # get hold of data from particular url
 response = requests.get(URL)
